2d612eb7af56d2c76f7f48c0cbe8c110d2bab010
merge-simpson <letsdev@kakao.com>
2022-10-19 23:26:46 +0900

    fix(routes): change each other

--- src/components/routes/ProtectedRoutes.tsx
+++ src/components/routes/ProtectedRoutes.tsx
@@ -1,9 +1,10 @@
+import Home from "@components/home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 const ProtectedRoutes = () => {
   return (
     <Routes>
-      <Route path="login" element={<div>Login Page</div>} />
+      <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate replace to="/login" />} />
     </Routes>
   );
--- src/components/routes/UnauthenticatedRoutes.tsx
+++ src/components/routes/UnauthenticatedRoutes.tsx
@@ -1,10 +1,9 @@
-import Home from "@components/home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 const UnauthenticatedRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="login" element={<div>Login Page</div>} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
