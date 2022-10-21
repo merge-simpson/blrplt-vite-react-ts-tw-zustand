495844f78959b490698dc2f67ee60b3341eec6b2
merge-simpson <letsdev@kakao.com>
2022-10-22 01:31:15 +0900

    chore(use ref effect): add docs

--- src/utils/useRefEffect.ts
+++ src/utils/useRefEffect.ts
@@ -3,6 +3,13 @@ import React, { useEffect } from "react";
 type T = React.EffectCallback;
 type P = React.DependencyList;
 
+/** useRefEffect(callback, [deps])
+ * tracks the change of value
+ * if using ref on input.
+ *
+ * @param callback
+ * @param deps
+ */
 const useRefEffect = (callback: T, deps?: P) => {
   useEffect(() => {
     if (!deps) {
