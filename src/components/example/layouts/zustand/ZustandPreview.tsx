5dc2b49af29560f72a3eed9bbf52e041a9b6b82f
merge-simpson <letsdev@kakao.com>
2022-10-22 04:46:40 +0900

    chore(ui preview): apply num input

--- src/components/example/layouts/zustand/ZustandPreview.tsx
+++ src/components/example/layouts/zustand/ZustandPreview.tsx
@@ -19,38 +19,43 @@ const ZustandPreview = () => {
       <h1 className="text-2xl font-bold">ZustandPreview</h1>
       <section className="flex flex-row gap-8">
         <article className="flex flex-col gap-8">
-          <fieldset className="flex justify-between gap-4">
+          <fieldset className="grid grid-cols-4 gap-4">
             <span className="font-bold">Num</span>
             <input
               readOnly
-              className="border bg-gray-100 text-right px-2 py-4 text-2xl"
+              className="col-span-3 border bg-gray-100 text-right px-2 py-4 text-2xl"
               value={store.num}
             />
           </fieldset>
           <DarkButton onClick={store.increase}>Increase</DarkButton>
         </article>
         <article className="flex flex-col gap-8 bg-gray-200 rounded-md shadow-inner p-4">
-          <fieldset className="flex justify-between gap-4">
+          <fieldset className="grid grid-cols-4 gap-4">
             <span className="font-bold">Target Num</span>
-            <IntegerInput
-              className="border bg-gray-100 text-right px-2 py-4 text-2xl"
-              onChange={(evt) => {
-                const targetNum = +(
-                  evt.target as HTMLInputElement
-                ).value.replace(/[^0-9\-]/g, "");
+            <div className="col-span-3">
+              <span>Integer</span>
+              <IntegerInput
+                className="border bg-gray-100 text-right px-2 py-4 text-2xl"
+                onChange={(evt) => {
+                  const targetNum = +(
+                    evt.target as HTMLInputElement
+                  ).value.replace(/[^0-9\-]/g, "");
 
-                setTargetNum(targetNum);
-              }}
-            />
-            <NumberInput
-              onChange={(evt) => {
-                const targetNum = +(
-                  evt.target as HTMLInputElement
-                ).value.replace(/[^0-9\-\.]/g, "");
+                  setTargetNum(targetNum);
+                }}
+              />
+              <span>Number</span>
+              <NumberInput
+                className="border bg-gray-100 text-right px-2 py-4 text-2xl"
+                onChange={(evt) => {
+                  const targetNum = +(
+                    evt.target as HTMLInputElement
+                  ).value.replace(/[^0-9\-\.]/g, "");
 
-                setTargetNum(targetNum);
-              }}
-            />
+                  setTargetNum(targetNum);
+                }}
+              />
+            </div>
           </fieldset>
           <DarkButton onClick={() => store.setNum(targetNum || 0)}>
             Apply
