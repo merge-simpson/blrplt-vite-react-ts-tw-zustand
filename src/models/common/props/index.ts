28523bfb1853bc279ed36e65f10662c45dfa2350
merge-simpson <letsdev@kakao.com>
2022-10-22 03:52:45 +0900

    fix(common props): input props rarely use class id

--- src/models/common/props/index.ts
+++ src/models/common/props/index.ts
@@ -3,7 +3,7 @@ import { HTMLProps as Props } from "react";
 type InitProps = Props<HTMLElement>;
 export default InitProps;
 
-export type CommonInputProps = Props<HTMLInputElement>;
+export type CommonInputProps = Omit<Props<HTMLInputElement>, "classID">;
 
 export type CommonDivProps = Omit<Props<HTMLDivElement>, "classID">;
 
