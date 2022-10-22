85f1fce792b9d552bc1625a6fc602f21a98bc457
merge-simpson <letsdev@kakao.com>
2022-10-22 13:00:06 +0900

    feat(web storage): create storage manager and key

---
+++ src/utils/common/web/storage/index.ts
@@ -0,0 +1,28 @@
+import STORAGE_KEY from "../storage-key";
+
+const storageManager = {
+  setItem: localStorage.setItem,
+
+  getItem: localStorage.getItem,
+
+  // 기기/브라우저 종속적 데이터가 있다면 STICKY하게 보존하고 다른 것만 지움.
+  clearAllUnsticky: () => {
+    type T = { [key: string]: string };
+    const stickyKeys = Object.keys(STORAGE_KEY.STICKY);
+    const stickyObject = Object.keys(localStorage).reduce<T>(
+      (acc, keyName) =>
+        stickyKeys.includes(keyName)
+          ? { ...acc, [keyName]: localStorage.getItem(keyName)! }
+          : acc,
+      {}
+    );
+
+    localStorage.clear();
+
+    Object.keys(stickyObject).forEach((keyName) =>
+      localStorage.setItem(keyName, stickyObject[keyName])
+    );
+  },
+};
+
+export default storageManager;
