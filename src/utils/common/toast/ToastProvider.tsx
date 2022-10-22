cbf58a242564445154804d8b0e5bd67dc5291761
merge-simpson <letsdev@kakao.com>
2022-10-22 22:56:50 +0900

    feat(toast): apply animation

--- src/utils/common/toast/ToastProvider.tsx
+++ src/utils/common/toast/ToastProvider.tsx
@@ -1,5 +1,6 @@
 import useToast from "@store/useToast";
 import { HTMLProps, FunctionComponent as FC } from "react";
+import { motion } from "framer-motion";
 
 export interface BrowserToastProps extends HTMLProps<HTMLDivElement> {
   //
@@ -12,9 +13,14 @@ const ToastProvider: FC<BrowserToastProps> = (props) => {
   return (
     <div>
       {toast.isOpen && (
-        <div className="fixed top-3/4 -translate-y-1/2 left-1/2 -translate-x-1/2 min-w-[50vw] bg-black bg-opacity-70 text-white rounded px-2 py-1 text-center">
+        <motion.div
+          className="fixed top-3/4 -translate-y-1/2 left-1/2 -translate-x-1/2 min-w-[50vw] bg-black bg-opacity-70 text-white rounded px-2 py-1 text-center"
+          initial={{ opacity: 0.5 }}
+          animate={{ opacity: 1.0 }}
+          transition={{ ease: "easeInOut", duration: 0.3 }}
+        >
           {toast.children}
-        </div>
+        </motion.div>
       )}
       {children}
     </div>
