c1d29cb4a768919d81a25c140df614baf4298f04
merge-simpson <letsdev@kakao.com>
2022-12-10 00:30:27 +0900

    fix(modal): prevent outer overflow

--- src/styles/modal/Modal.tsx
+++ src/styles/modal/Modal.tsx
@@ -59,14 +59,20 @@ export const ModalProvider: FC<ModalProviderProps> = ({ children: app }) => {
     setOpen(false);
   }, [location]);
 
+  useLayoutEffect(() => {
+    const currntOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = currntOverflow;
+    };
+  }, []);
+
   return (
     <div className="relative z-0 min-h-screen">
       <div className="z-0">{app}</div>
       {isOpen && (
-        <aside className="absolute left-0 right-0 top-0 bottom-0 bg-black bg-opacity-50 z-10" />
-      )}
-      {isOpen && (
-        <aside className="absolute left-0 right-0 top-0 bottom-0 z-20">
+        <aside className="absolute left-0 top-0 w-screen h-screen bg-black bg-opacity-50 z-10">
           {modal}
         </aside>
       )}
